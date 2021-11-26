20f4c1e4fadba31b47c6c33fc7ecffb0379b28bf
josealvarez97 <alvarez.jo.developer@gmail.com>
2021-11-26 03:32:42 +0000

    Parametrize heat equation cloud function

--- diffeq-cloud-functions/pde-heat-equation/main.py
+++ diffeq-cloud-functions/pde-heat-equation/main.py
@@ -5,9 +5,62 @@ import os
 from os.path import basename
 from io import BytesIO
 
+from math import *
+
+def parse_parameter(request, param):
+    request_json = request.get_json()
+    print("param", param)
+    print("request json", request_json)
+    if request.args and param in request.args:
+        return request.arg.get(param)
+    elif request_json and param in request_json:
+        return request_json[param]
+    else:
+        raise ValueError(f"Body is invalid, or missing '{param}' property")
+
+def parse_function_parameter(request, param):
+    print("parse_function_parameter")
+    request_json = request.get_json()
+    if request.args and param in request.args:
+        exec(param + "=" + request.args.get(param), globals())
+        # f = locals()['f']
+    elif request_json and param in request_json:
+        exec(param + "="+ request_json[param], globals())
+        # f = locals()['f']
+    else:
+        raise ValueError(f"Body is invalid, or missing '{param}' property")
+    print("success with parse_function_parameter")
+    return globals()[param]
+    
+
 def heat_equation(request):
+    # Parse parameters
+    request_json = request.get_json()
+    
+    heat_x_0 = parse_parameter(request, 'heat_x_0')
+    heat_x_max = parse_parameter(request, 'heat_x_max')
+    alpha = parse_parameter(request, 'alpha')
+    Nx = parse_parameter(request, 'Nx')
+    x_max = parse_parameter(request, 'x_max')
+    Mt = parse_parameter(request, 'Mt')
+    t_max = parse_parameter(request, 't_max')
+    f_0 = parse_function_parameter(request, 'f_0')
+    
+    
+    # L = 1
+    # f_0 = lambda x: sin(pi*x/L)
+    # phi_0 = phi_L = 0
+    # alpha=0.1
+    
+    
     # Run simulation
-    solver = HeatEquationSolver(save_plots=True)
+    solver = HeatEquationSolver(f_0=f_0, 
+                                heat_x_0=heat_x_0,
+                                heat_x_max=heat_x_max,
+                                alpha=alpha,
+                                Nx=Nx, x_max=x_max, 
+                                Mt=Mt, t_max=t_max,
+                                save_plots=True)    
     u_solution = solver.get_solution()
     solver.plot_stacked(u_solution)
     
